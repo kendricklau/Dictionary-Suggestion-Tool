e4cc00bd53fd6519a8cb6c771953822fa01f4163
doublefloyd <ebartusi@purdue.edu>
2016-04-28 02:06:00 -0400

    spell corrector2

--- readexcel.py
+++ readexcel.py
@@ -13,18 +13,19 @@ def train(features):
         model[f] += 1
     return model
 
-NWORDS = train(words(file('big.txt').read()))
+NWORDS = train(words(file('data.txt').read()))
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 #performs various operations on input in order to try to correct word
 def edits1(word):
    splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
-   deletes    = [a + b[1:] for a, b in splits if b]
+   #deletes    = [a + b[1:] for a, b in splits if b]
    transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
-   replaces   = [a + c + b[1:] for a, b in splits for c in alphabet if b]
-   inserts    = [a + c + b     for a, b in splits for c in alphabet]
-   return set(deletes + transposes + replaces + inserts)
+   #replaces   = [a + c + b[1:] for a, b in splits for c in alphabet if b]
+   #inserts    = [a + c + b     for a, b in splits for c in alphabet]
+   return set(transposes)
+   #return set(deletes + transposes + replaces + inserts)
 
 #two edits
 def known_edits2(word):
@@ -32,9 +33,21 @@ def known_edits2(word):
 
 #one edit
 def known(words):
-    return set(w for w in words if w in NWORDS)
+    emily = set()
+    for n in words:
+        for i in NWORDS:
+            if i.startswith(n):
+                emily.add(n)
+                break
+    return emily
+    #return set(w for w in words if w in NWORDS)
 
 #final function that returns correction or original word
 def correct(word):
+    #candidates = known([word]) or known(edits1(word)) or [word]
     candidates = known([word]) or known(edits1(word)) or known_edits2(word) or [word]
     return max(candidates, key=NWORDS.get)
+
+text = raw_input('Enter word here: ')
+best = correct(text)
+print(best)
